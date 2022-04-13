b04cf3b8d8540a3d02240dbd5f8d2f161662481a
JBUinfo <37612885+JBUinfo@users.noreply.github.com>
2022-04-13 15:30:58 +0200

    Update readCSV.py

--- readCSV.py
+++ readCSV.py
@@ -1,4 +1,4 @@
 import pandas as pd
-def readCSV_Normalize(coin, timeFrame)
+def readCSV_Normalize(coin, timeFrame):
     cols = ['OpenTime', 'Open', 'High', 'Low', 'Close', 'Volume', 'CloseTime', 'QuoteAssetVolume', 'NumberOfTrades', 'TakerBuyBaseAssetVolume', 'TakerBuyQuoteAssetVolume', 'Ignore']
     return pd.read_csv(f"./coins/{coin}/{timeFrame}/{timeFrame}.csv", names=cols, header=0)
